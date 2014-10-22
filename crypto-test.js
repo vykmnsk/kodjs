44c8b98b104b2b27d3b887f730f45c99f3e8262b
Yuri Klind <vykmnsk@gmail.com>
2014-10-22 22:03:38 +1100

    add coding in prompt logic

--- crypto-test.js
+++ crypto-test.js
@@ -4,8 +4,8 @@ var CryptoJS = require('./node_modules/crypto-js');
 
 var pwd = "Secret Passphrase";
 var encrypted = CryptoJS.AES.encrypt("Test Message", pwd);
-var enc_msg = encrypted.toString(CryptoJS.enc.Utf8);
-console.log(enc_msg);
+// var enc_msg = encrypted.toString(CryptoJS.enc.Utf8);
+console.log(encrypted.toString());
 
 var decrypted = CryptoJS.AES.decrypt(encrypted.toString(), pwd)
 var decr_msg = decrypted.toString(CryptoJS.enc.Utf8);
--- prompt_pwd.js
+++ prompt_pwd.js
@@ -1,15 +1,37 @@
 /**
  * Encoding/Decoding
  */
-var fs = require('fs');
 
-"use strict";
+var fs = require('fs');
+var CryptoJS = require('./node_modules/crypto-js');
+
+"use strict"
+
 var inquirer = require("./node_modules/inquirer/lib/inquirer");
-var questions = JSON.parse(fs.readFileSync('questions.json', 'utf8'))
+var questions = JSON.parse(fs.readFileSync('questions.json', 'utf8'));
+
+function encode(text, pwd) {
+    var encrypted = CryptoJS.AES.encrypt("Test Message", pwd);
+    console.log(encrypted.toString());
+}
+
+function decode(code, pwd) {
+  var decrypted = CryptoJS.AES.decrypt(code, pwd);
+  var decr_msg = decrypted.toString(CryptoJS.enc.Utf8);
+  console.log(decr_msg);
+}
 
 inquirer.prompt(questions, function( answers ) {
   console.log( JSON.stringify(answers, null, "  ") );
-  if (answers.doit) {
-    console.log("Running...");
+
+  if ('encode' == answers.operation) {
+    var text = 'Sovershenno Sekretno!';
+    encode(text, answers.pwd);
+  } else if ('decode' == answers.operation) {
+    var code = 'U2FsdGVkX18+puhcxaXIqts2jJD7TxDpRvNrAdAbnow=';
+    decode(code, answers.pwd);
+  } else {
+    throw 'Err! Unknownd operation';
   }
+
 });

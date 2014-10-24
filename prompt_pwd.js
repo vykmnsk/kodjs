1b9b75efe0c0131e9060b6a8d2bdcf38138e7908
Yuri Klind <vykmnsk@gmail.com>
2014-10-24 20:28:19 +1100

    encoding inside prompt, basic requirements met

--- prompt_pwd.js
+++ prompt_pwd.js
@@ -4,32 +4,64 @@
 
 var fs = require('fs');
 var CryptoJS = require('./node_modules/crypto-js');
+var ENC_EXT = 'kod'
 
 "use strict"
 
 var inquirer = require("./node_modules/inquirer/lib/inquirer");
 var questions = JSON.parse(fs.readFileSync('questions.json', 'utf8'));
 
-function encode(text, pwd) {
-    var encrypted = CryptoJS.AES.encrypt("Test Message", pwd);
-    console.log(encrypted.toString());
+function readFileData(file, callback, callParams) {
+  console.log("reading %s ...", file);
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) throw err;
+    callback.apply(null, [data].concat(callParams));
+    });
+}
+
+function writeFileData(file, data) {
+  console.log("writing %s ...", file);
+  fs.writeFile(file, data, function (err) {
+    if (err) throw err;
+    });
+}
+
+function replaceFileExt(fileIn, newExt) {
+  var fileParts = fileIn.split(".");
+  if (fileParts.length < 2) throw "Problem with input file path: " + fileIn;
+  fileParts[fileParts.length -1] = newExt;
+  fileOut = fileParts.join('.');
+  return fileOut;
+}
+
+function encode(text, pwd, file) {
+  var encrypted = CryptoJS.AES.encrypt(text, pwd);
+  var encText = encrypted.toString()
+  writeFileData(replaceFileExt(file, ENC_EXT), encText)
+  logFormatted(encText)
 }
 
 function decode(code, pwd) {
   var decrypted = CryptoJS.AES.decrypt(code, pwd);
-  var decr_msg = decrypted.toString(CryptoJS.enc.Utf8);
-  console.log(decr_msg);
+  var decrText = decrypted.toString(CryptoJS.enc.Utf8);
+  logFormatted(decrText);
+}
+
+function logFormatted(msg){
+  console.log("--------------------------------------------");
+  console.log(msg);
+  console.log("--------------------------------------------");
 }
 
 inquirer.prompt(questions, function( answers ) {
   console.log( JSON.stringify(answers, null, "  ") );
 
   if ('encode' == answers.operation) {
-    var text = 'Sovershenno Sekretno!';
-    encode(text, answers.pwd);
+    // var text = 'Sovershenno Sekretno!';
+    // encode(text, answers.pwd);
+    readFileData(answers.file, encode, [answers.pwd, answers.file]);
   } else if ('decode' == answers.operation) {
-    var code = 'U2FsdGVkX18+puhcxaXIqts2jJD7TxDpRvNrAdAbnow=';
-    decode(code, answers.pwd);
+    readFileData(answers.file, decode, [answers.pwd]);
   } else {
     throw 'Err! Unknownd operation';
   }
